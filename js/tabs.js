df0958df41d02084761cc2f0153452d2726edead
lukashevich-vitaly <lykashevich.vitalij@gmail.com>
2021-11-27 01:05:53 +0300

    feat: add tabs js

---
+++ js/tabs.js
@@ -0,0 +1,28 @@
+const tabs = (option) => {
+    const {
+        selectorTabsButton,
+        activeClassButton,
+        selectorTabsElement,
+        activeClassTab,
+        callback = () => {},
+    } = option
+
+    const tabsBtns = document.querySelectorAll(selectorTabsButton)
+    const tabsElems = document.querySelectorAll(selectorTabsElement)
+
+    tabsBtns.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            for (let i = 0; i < tabsBtns.length; i++){
+                if(tabsBtns[i] === btn) {
+                    tabsBtns[i].classList.add(activeClassButton)
+                    tabsElems[i].classList.add(activeClassTab)
+                } else {
+                    tabsBtns[i].classList.remove(activeClassButton)
+                    tabsElems[i].classList.remove(activeClassTab)
+                }
+            }
+        })
+    })
+}
+
+export default tabs
