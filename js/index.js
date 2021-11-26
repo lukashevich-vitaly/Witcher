88d5ff29fedeec40f885d361f392f967134154e6
lukashevich-vitaly <lykashevich.vitalij@gmail.com>
2021-11-26 23:06:17 +0300

    feat: add burger js finish

--- js/index.js
+++ js/index.js
@@ -2,5 +2,5 @@ import burgerMenu from "./burger.js";
 
 burgerMenu({
     selectorMenu: '.navigation__list',
-    openMenuSelectir: '.navigation__list-active'
+    openMenuSelector: 'navigation__list_active'
 })
